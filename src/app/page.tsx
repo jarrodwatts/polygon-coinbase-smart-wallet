f6c6b3d2b25e167c2a323511107e3d3e68eff17b
Jarrod Watts <jarrodwatts16@gmail.com>
2024-06-07 10:38:43 +1000

    comments in code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,56 @@
-// File: /src/app/page.tsx
 "use client"; // thirdweb SDK doesn't support server-side rendering yet.
 
 import { createThirdwebClient, getContract } from "thirdweb"; // Create the thirdweb client with your client ID
 import {
-  ConnectButton,
-  TransactionButton,
-  useActiveAccount,
+  ConnectButton, // Connect wallet button and modal
+  TransactionButton, // Button to send transactions from the connected wallet
+  useActiveAccount, // Hook to grab the connected account
 } from "thirdweb/react"; // The component we can prompt the user to connect their wallet with
 import { createWallet } from "thirdweb/wallets"; // Function to specify we want to use Coinbase smart wallet
-import { polygon } from "thirdweb/chains"; // Import the the blockchain you want to use
-import { claimTo } from "thirdweb/extensions/erc1155";
+import { claimTo } from "thirdweb/extensions/erc1155"; // Function to claim a
 import { useSendCalls } from "thirdweb/wallets/eip5792";
 
+// Import and set the the blockchain you want to use.
+// Only certain chains supported: https://www.smartwallet.dev/FAQ#what-networks-are-supported
+import { polygon } from "thirdweb/chains";
+const chainToUse = polygon;
+
 const thirdwebClient = createThirdwebClient({
+  // Grab your thirdweb client id from the thirdweb dashboard
+  //Ensure you have a .env.local file with NEXT_PUBLIC_THIRDWEB_CLIENT_ID set to your client ID
   clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID!,
 });
 
-const chainToUse = polygon;
-
 export default function Home() {
+  // Get the account once the user has connected with CB smart wallet
   const account = useActiveAccount();
 
+  // This is so we can use a paymaster service to pay for the transaction gas fees
   const { mutateAsync: sendCalls } = useSendCalls({
     client: thirdwebClient,
     waitForResult: true,
   });
 
+  // Function to send the claim NFT transaction with gas fees covered.
   async function sendSponsoredTransaction() {
     const claimTx = claimTo({
+      // Get the contract using a chain + contract address combo
       contract: getContract({
         client: thirdwebClient,
         chain: chainToUse,
-        address: "0x04bE3B7a1a034297331A661c5b5E838264Ba0E58",
+        address: "0x04bE3B7a1a034297331A661c5b5E838264Ba0E58", // View contract: https://thirdweb.com/polygon/0x04bE3B7a1a034297331A661c5b5E838264Ba0E58
       }),
-      quantity: BigInt(1),
-      to: account?.address!,
-      tokenId: BigInt(0),
+      quantity: BigInt(1), // Claim 1 NFT
+      to: account?.address!, // To the connected wallet address
+      tokenId: BigInt(0), // Of NFT token ID 0
     });
 
+    // Send the transaction with the paymaster service
     return await sendCalls({
-      calls: [claimTx],
+      calls: [claimTx], // The claim transaction. We could put multiple transactions here in theory.
       capabilities: {
         paymasterService: {
+          // Docs: https://portal.thirdweb.com/connect/account-abstraction/infrastructure
           url: `https://${chainToUse.id}.bundler.thirdweb.com/${thirdwebClient.clientId}`,
         },
       },
@@ -70,6 +79,7 @@ export default function Home() {
             },
             // What chains we want to support in our app
             chains: [chainToUse],
+            // This is the metadata that shows up when prompting the user to connect their wallet
             appMetadata: {
               logoUrl: `https://github.com/jarrodwatts/polygon-coinbase-smart-wallet/blob/main/public/wizard-hat.png?raw=true`,
               name: "Polygon 💜🤝 Coinbase Smart Wallet",
@@ -80,7 +90,7 @@ export default function Home() {
       />
       <TransactionButton
         transaction={() =>
-          // @ts-expect-error
+          // @ts-expect-error: Just a type issue, can probably be fixed easily
           sendSponsoredTransaction()
         }
         payModal={false}
